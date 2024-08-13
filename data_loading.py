eb06ee0734af683c0d7dac8b7fdc8ffe461222b7
Masumi Yano <92832478+MasumiYano@users.noreply.github.com>
2024-08-12 18:09:13 -0700

    Data loader created. Models created.

--- data_loading.py
+++ data_loading.py
@@ -1,31 +1,28 @@
-import torch
-import torchvision.transforms as transforms
-from torch.utils.data import DataLoader
-from torchvision.transforms import ToTensor
 import os
-from torchvision.io import read_image
+import torch
+from torch.utils.data import Dataset
 from torchvision import datasets
+import torchvision.transforms as transforms
 
 
-class PoemDataset:
-    def __init__(self, poem_dir, transform=None, target_transform=None):
+class PoemDataLoader(Dataset):
+    def __init__(self, poem_dir, transform=None):
+        super(PoemDataLoader, self).__init__()
         self.poem_dir = poem_dir
         self.transform = transform
-        self.target_transform = target_transform
-        self.poem_files = []
+        self.poem_index = []
 
         for root, _, files in os.walk(self.poem_dir):
             for file in files:
-                self.poem_files.append(os.path.join(root, file))
+                self.poem_index.append(str(os.path.join(self.poem_dir, file)))
 
     def __len__(self):
-        return len(self.poem_dir)
+        return len(self.poem_index)
 
     def __getitem__(self, idx):
-        poem_path = self.poem_files[idx]
-
-        with open(poem_path, 'r') as file:
-            poem = file.read()
+        poem_path = self.poem_index[idx]
+        with open(poem_path, 'r') as f:
+            poem = f.read()
 
         if self.transform:
             poem = self.transform(poem)
@@ -35,21 +32,11 @@ class PoemDataset:
 
 transform = transforms.Compose([
     transforms.Resize([224, 224]),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    transforms.ToTensor()
 ])
 
-
-coco_train_data = datasets.CocoCaptions(
-    root='coco_data/train/images',
-    annFile='coco_data/train/annotations',
+cap = datasets.CocoCaptions(
+    root='cocodataset/images',
+    annFile='cocodataset/annotation',
     transform=transform
 )
-
-coco_test_data = datasets.CocoCaptions(
-    root='coco_data/test/images',
-    annFile='coco_data/test/annotations',
-    transform=transform
-)
-
-
